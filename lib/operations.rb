cea38ebbd8d921a31a934b58ce5587659ab598b9
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-09 18:07:32 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,8 +11,8 @@ end
 def not_safe?(speed)
   if speed == 40 || 60
     false
-  elsif speed > 60 ? true : false
-  else speed < 40 ? true : false
+  elsif speed > 60 ? false : true
+  else speed < 40 ? false : true
   end  
 end
 	
